32868676c759b3879f83bccd6a3d77f2f5e64a83
Alex <133679349+alexmen656@users.noreply.github.com>
2025-10-26 20:56:59 +0100

    feat: Add logo generator

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import GameView from '../views/GameView.vue'
+import LogoGen from '../views/logo_generator.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/game',
-    name: 'game',
-    component: GameView
-  }
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/game',
+        name: 'game',
+        component: GameView
+    },
+    {
+        path: '/logo_gen',
+        name: 'logo',
+        component: LogoGen
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+    history: createWebHistory(),
+    routes
 })
 
 export default router
---
+++ src/views/logo_generator.vue
@@ -0,0 +1,63 @@
+<template>
+    <canvas ref="nestCanvasRef" width="400" height="400" class="nest-canvas"></canvas>
+</template>
+
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const nestCanvasRef = ref(null)
+
+
+let nestCtx = null
+
+function setupCanvas() {
+    console.log('Setting up canvas')
+    const canvas = nestCanvasRef.value
+    if (!canvas) return
+    nestCtx = canvas.getContext('2d')
+    return nestCtx;
+}
+
+    const centerX = 200
+    const centerY = 220
+
+
+    function drawWoodcock(x, y) {
+      const ctx = setupCanvas()
+    ctx.save()
+    ctx.fillStyle = '#8B6F47'
+    ctx.beginPath()
+    ctx.ellipse(x, y, 25, 20, 0, 0, Math.PI * 2)
+    ctx.fill()
+
+    ctx.fillStyle = '#A0826D'
+    ctx.beginPath()
+    ctx.ellipse(x + 15, y, 12, 15, 0.3, 0, Math.PI * 2)
+    ctx.fill()
+
+    ctx.fillStyle = '#8B6F47'
+    ctx.beginPath()
+    ctx.arc(x - 15, y - 10, 15, 0, Math.PI * 2)
+    ctx.fill()
+
+    ctx.fillStyle = '#CD853F'
+    ctx.beginPath()
+    ctx.moveTo(x - 15, y - 10)
+    ctx.lineTo(x - 35, y - 12)
+    ctx.lineTo(x - 35, y - 8)
+    ctx.closePath()
+    ctx.fill()
+
+    ctx.fillStyle = 'black'
+    ctx.beginPath()
+    ctx.arc(x - 12, y - 13, 2, 0, Math.PI * 2)
+    ctx.fill()
+
+    ctx.restore()
+}
+
+ onMounted(() => {
+  drawWoodcock(centerX - 30, centerY - 50)
+})
+</script>
